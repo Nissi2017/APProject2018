272e4b18b1adde3b384580e56e1496d7e09d786d
ExCharmer <36269261+ExCharmer@users.noreply.github.com>
2018-02-18 17:30:03 -0500

    Add files via upload

--- CustomerDashboard.java
+++ CustomerDashboard.java
@@ -1,10 +1,8 @@
 package view;
 
-import java.awt.Container;
-import java.awt.Font;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,9 +13,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
-
 import controller.CustController;
 import controller.FarmController;
 
@@ -30,13 +26,11 @@ public class CustomerDashboard  extends JFrame implements ActionListener
 	private JMenuBar menuBar = new JMenuBar();
 	//private JMenu ViewProfile = new JMenu("View Profile");
 	private JMenu addFunds = new JMenu("Add Funds");
-	
 	private JPanel profilePanel = new JPanel();
 	private JPanel contentPanel = new JPanel();
 	private JLabel prPic = new JLabel("");
 	private JTextField fullname = new JTextField();
 	private JLabel fullnameL = new JLabel("Full Name");
-	private JLabel addrL = new JLabel("Address");
 	private  JTextField addr = new JTextField();
 	private  JLabel emailL = new JLabel("Email");
 	private  JTextField email = new JTextField();
@@ -49,52 +43,47 @@ public class CustomerDashboard  extends JFrame implements ActionListener
 	private JMenu mnExit = new JMenu("Exit");
 	private JMenuItem mntmLogout = new JMenuItem("Logout");
  
+	public CustomerDashboard() {
+		configureView();
+		ConfigureListeners ();
+	}
+	
+
 	public void configureView()
 	{	
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPanel);
 		contentPanel.setLayout(null);
-		profilePanel.setBounds(0, 22, 474, 287);
-		
+		setBounds(100, 100, 490, 348);
 		/*contentPanel.add(profilePanel);
 		profilePanel.setLayout(null);
 		prPic.setBounds(255, 63, 209, 170);
-
 		profilePanel.add(prPic);
 		fullname.setEditable(false);
 		fullname.setBackground(UIManager.getColor("TextField.inactiveBackground"));
 		fullname.setColumns(10);
 		fullname.setBounds(101, 63, 111, 20);
-
 		profilePanel.add(fullname);
 		fullnameL.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		fullnameL.setBounds(10, 66, 71, 14);
-
 		profilePanel.add(fullnameL);
 		addrL.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		addrL.setBounds(10, 197, 71, 14);
-
 		profilePanel.add(addrL);
-
 		profilePanel.add(addr);
 		emailL.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		emailL.setBounds(10, 112, 46, 14);
-
 		profilePanel.add(emailL);
 		email.setEditable(false);
-
 		profilePanel.add(email);
 		totalL.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		totalL.setBounds(10, 156, 81, 14);
-
 		profilePanel.add(totalL);
-
 		profilePanel.add(total);
 		profilePanel.setVisible(false);*/
 		contentPanel.add(menuBar);
-		//setTitle("Welcome to the customer Dashboard");
-		contentPanel.setVisible(true);
-		contentPanel.setSize(1200,600);
+		//setTitle("Welcome to the Customer Dashboard");
+			contentPanel.setSize(1200,600);
 		cartbutton.setIcon(new ImageIcon("C:\\Users\\odane\\Downloads\\APProject2018-master\\FarmersMarket\\download.png"));
 		menuBar.setBounds(0, 0, 488, 21);
 		menuBar.add(ManageCrops);
@@ -104,12 +93,22 @@ public class CustomerDashboard  extends JFrame implements ActionListener
 		menuBar.add(mnExit);
 		menuBar.add(addFunds);
 		mnExit.add(mntmLogout);
-		
-		
+		menuBar.setVisible(true);
+		contentPanel.setVisible(true);
 		
 	}
-@Override
-	public void actionPerformed(ActionEvent e) {
+
+	public void ConfigureListeners () {
+		mntmLogout.addActionListener(this);
+		addFunds.addActionListener(this);
+		ViewAll.addActionListener(this);
+		ViewSome.addActionListener(this);
+		cartbutton.addActionListener(this);
+	}
+	
+	@Override
+
+public void actionPerformed(ActionEvent e) {
 
 		//EXITS ACCOUNT
 		if (e.getSource().equals(mntmLogout)) {
@@ -152,4 +151,3 @@ public class CustomerDashboard  extends JFrame implements ActionListener
 	// TODO Auto-generated method stub
 	
 }
-
